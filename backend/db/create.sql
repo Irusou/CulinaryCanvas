d52e5a0841d51450701cd6bb5ba2cb699efc25ed
João Pedro <irus0ubusiness@gmail.com>
2024-01-29 18:17:13 +0000

    alt: updated create, populate and index(pf)
    i swear it's the last commit..

--- backend/db/create.sql
+++ backend/db/create.sql
@@ -1,3 +1,4 @@
+drop database CulinaryCanvas;
 create database CulinaryCanvas;
 use CulinaryCanvas;
 create table if not exists Mesa(
@@ -5,24 +6,24 @@ create table if not exists Mesa(
   isOpen boolean default true 
 );
 
-create table if not exists Produto(
-	id int primary key auto_increment,
-  description varchar(50) not null,
-  price float not null,
-  tipo int not null,
-  foreign key (tipo) references Tipo(id)
-);
-
 create table if not exists Tipo(
 	id int primary key auto_increment,
   description varchar(50) not null
 );
 
-create table if not exists ProdutoMesa(
+create table if not exists Produto(
+	id int primary key auto_increment,
+  description varchar(50) not null,
+  price float not null,
+  tipo int not null,
+  foreign key (tipo) references Tipo(id) on delete cascade on update cascade
+);
+
+create table if not exists Pedido(
+	id int primary key auto_increment,
 	mesa int not null,
   produto int not null,
   quantity int not null default 0,
-  primary key(mesa, produto),
-  foreign key (mesa) references Mesa(id),
-  foreign key (produto) references Produto(id)
+  foreign key (mesa) references Mesa(id)on delete cascade on update cascade,
+  foreign key (produto) references Produto(id)on delete cascade on update cascade
 );
--- backend/db/populate.sql
+++ backend/db/populate.sql
@@ -1,10 +1,8 @@
--- POPULATE TABLE `Tipo`
 insert into Tipo(description) values("Bebida");
 insert into Tipo(description) values("Entrada");
 insert into Tipo(description) values("Prato Principal");
 insert into Tipo(description) values("Sobremesa");
 
--- POPULATE TABLE `Mesa`
 insert into Mesa(isOpen) values(true);
 insert into Mesa(isOpen) values(true);
 insert into Mesa(isOpen) values(true);
@@ -26,7 +24,6 @@ insert into Mesa(isOpen) values(true);
 insert into Mesa(isOpen) values(true);
 insert into Mesa(isOpen) values(true);
 
--- POPULATE TABLE `Produto`
 insert into Produto(description, price, tipo) values("Arroz de Marisco",15,3);
 insert into Produto(description, price, tipo) values("Choco Frito",10,3);
 insert into Produto(description, price, tipo) values("Arroz Doce",2.5,4);
